1644704fd86d78af376c2e9a3086bd4a0ac7b868
caopei <caopei@hxxn.onaliyun.com>
2017-11-16 12:39:52 +0800

    modify

--- src/main/java/com/emcloud/loc/repository/AddressRepository.java
+++ src/main/java/com/emcloud/loc/repository/AddressRepository.java
@@ -1,6 +1,8 @@
 package com.emcloud.loc.repository;
 
 import com.emcloud.loc.domain.Address;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
 import org.springframework.data.jpa.repository.*;
@@ -12,5 +14,5 @@ import org.springframework.data.jpa.repository.*;
 @SuppressWarnings("unused")
 @Repository
 public interface AddressRepository extends JpaRepository<Address, Long> {
-
+     Page<Address> findAllByAddressNameContaining(Pageable pageable,String addressName);
 }
